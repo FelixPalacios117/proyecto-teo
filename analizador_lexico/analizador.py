4beeba9cabafecf197f0661cca4bbc330847f2bf
FelixPalacios117 <00055419@uca.edu.sv>
2023-10-01 00:07:01 -0600

    changes

--- analizador_lexico/analizador.py
+++ analizador_lexico/analizador.py
@@ -1,50 +1,69 @@
 import ply.lex as lex
-import re
 from utils.reservadas import reservadas
 from utils.tokens import *
 
-##tokens=tokens+list(reservadas.values())
 t_ignore=' \t'
-t_plus=r'\+'
-t_assign=r'='
-t_minus=r'\-'
-t_multiplication=r'\*'
+t_mas=r'\+'
+t_asignacion=r'='
+t_menos=r'\-'
+t_multiplicacion=r'\*'
 t_division=r'/'
-t_comma=r','
-t_semicolon=r';'
-t_leftkey=r'\{'
-t_rightkey=r'\}'
-t_leftparent=r'\('
-t_rightparent=r'\)'
+t_coma=r','
+t_punto_y_coma=r';'
+t_llave_izquierda=r'\{'
+t_llave_derecha=r'\}'
+t_parentesis_izquierdo=r'\('
+t_parentesis_derecho=r'\)'
 t_namespace=r'namespace'
+t_insercion = r'<<'
+t_extraccion = r'>>'
+
+def t_char(token):
+    r"'.'"
+    return token
+def t_string(token):
+    r'"[^"]*"'
+    return token
+def t_bool(token):
+    r'true|false'
+    return token
 # Manejo de saltos de línea
-def t_newline(token):
+def t_salto_de_linea(token):
     r'\n+'
     token.lexer.lineno += len(token.value)
-def t_id(token):
+#nombre de la variable
+def t_identificador(token):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
-    token.type = reservadas.get(token.value, 'id')
+    token.type = reservadas.get(token.value, 'identificador')
     return token
-def t_comment(token):
+#comentario
+def t_comentario(token):
     r'\/\/.*'
     return token
-def t_comment_line(token):
+#linea de comentario
+def t_linea_de_comentario(token):
     r'\/\*[\s\S]*?\*\/'
     return token
+#Tipo double
+def t_double(token):
+    r'\d+\.\d+'
+    token.value = float(token.value)
+    return token
+#enteros
 def t_int(token):
     r'\d+'
     token.value=int(token.value)
-def t_float(token):
-    r'\d+(\.\d+)?'
-    token.value = float(token.value)
     return token
+#return
 def t_return(token):
-    r'return\s+\d+;'
+    r'return'
     return token
-def t_include(token):
+#Para incluir librerias
+def t_inclusion(token):
     r'\#include\s+[<"][\w\.]+[>"]'
-    token.type = reservadas.get(token.value, 'include')
+    token.type = reservadas.get(token.value, 'inclusion')
     return token
+#El manejo de errores(simbolos que no pertenecen al lenguaje)
 def t_error(t):
     print(f"Error léxico: '{t.value}'")
     t.lexer.skip(1)
@@ -54,12 +73,14 @@ def leer_fichero():
     with open('./test/test.cpp', 'r') as archivo_cpp:
     # Lee el contenido del archivo
         contenido=archivo_cpp.read()
-    print(contenido)
     return contenido
 # correr lexer
 if __name__ == '__main__':
     lexer.input(str(leer_fichero()))
-    print('archivo cargado')
+    print('Archivo cargado correctamente')
+    print('\n')
+    print('Lista de tokens')
+    print ('\n')
     while True:
         token = lexer.token()
         if not token:
--- analizador_lexico/test/test.cpp
+++ analizador_lexico/test/test.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
 int main(){
-    double x=1.1;
+    4os
+    4bxal=s;
+    bool a=true;
+    string p="sasasa";
+    char q='a';
+    double x=0.1;
     int c=0;
     //ok
     /* ssss */
