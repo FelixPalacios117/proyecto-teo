ea01c1e2750440bbcf45aa340e0b44ea8091aa8a
felixpalacios117 <00055419@uca.edu.sv>
2023-11-18 15:42:07 -0600

    Estructura base

--- analizador.py
+++ analizador.py
@@ -1,7 +1,7 @@
 import ply.lex as lex
 from utils.reservadas import reservadas
 from utils.tokens import *
-
+from utils.Nterminales import * #No terminales
 
 def t_identificador(token):
     r'([a-z]|[A-Z]|_)([a-z]|[A-Z]|\d|_)*'
@@ -12,12 +12,20 @@ def t_reservada(token):
 
 #ignora tabs y espacios
 t_ignore  = ' \t'
+#fin del archivo
+t_eof= r'\$'
+def t_comentario(t):
+    r'\/\/.*'
 
+def t_comentario_bloque(t):
+    r'\/\*(.|\n)*\*\/'
+ 
 #Manejo de errores
 def t_error(token):
     print("Illegal character '%s'" % token.value[0])
     token.lexer.skip(1)    
     return token
+
     
 if __name__ == '__main__':
     ##descomentar para testear con otro archivo de c
