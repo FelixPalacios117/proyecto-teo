e781fb0276942db7d7d900e193960046a7a7b154
felixpalacios117 <00055419@uca.edu.sv>
2023-11-22 11:34:24 -0600

    Fixes

--- test/prueba.c
+++ test/prueba.c
@@ -1,16 +1,30 @@
-/*int a, b = 0;
-asas*/
+ int a, b = 0;
 
-for (int a = 0; a < 2; a++)
+ for (int a = 0; a < 2; a++)
 {
+    for (int b = 0; b < 3; b--)
+    {
+        for (int c = 0; c < 3; c--)
+        {
+            a=0;
+        }
+    }
+    a=0;
+} 
+for(int a=0;a<20;a++){
+    
 }
 /* if (b <= c)
 {
+
 }
 else
 {
     a = 0;
 }
+int a=0;
+float cs=2.8; */
+/*
 if (b < a)
 {
     int c = 0;
